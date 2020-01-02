dba66e5c6f76e104df454f3585ee94e22b69e5b8
micheal <michealdunne14@gmail.com>
2020-01-02 20:27:52 +0000

    Bottom navigation bar

--- app/src/main/java/com/example/internetcookbook/PagerFragment.kt
+++ app/src/main/java/com/example/internetcookbook/PagerFragment.kt
@@ -1,16 +1,17 @@
 package com.example.internetcookbook
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import android.transition.TransitionInflater
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.FragmentManager
+import androidx.viewpager.widget.ViewPager
 import com.example.internetcookbook.adapter.TabsPagerAdapter
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.fragment_pager.*
+import kotlinx.android.synthetic.main.fragment_pager.view.*
+import kotlinx.android.synthetic.main.fragment_pager.view_pager
 import java.io.Serializable
 
 class PagerFragment : Fragment() {
@@ -27,6 +28,7 @@ class PagerFragment : Fragment() {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_pager, container, false)
         handleTransition()
+        initTabLayout(view)
         return view
     }
 
@@ -34,7 +36,6 @@ class PagerFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initAdapter()
-        initTabLayout()
     }
 
     private fun handleTransition() {
@@ -56,9 +57,61 @@ class PagerFragment : Fragment() {
             }
         }
     }
+    lateinit var pagerAdapter: TabsPagerAdapter
 
-    private fun initTabLayout() {
-        tab_layout.setupWithViewPager(view_pager)
+    private fun initTabLayout(view: View) {
+        view.navigationView.selectedItemId = R.id.mNavHome
+        pagerAdapter = TabsPagerAdapter(activity!!.supportFragmentManager)
+        val viewPager = view.view_pager
+        viewPager.adapter = pagerAdapter
+        viewPager.currentItem = 1
+
+        viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {}
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+            }
+
+            override fun onPageSelected(position: Int) {
+                when (position) {
+                    0 -> {
+                        navigationView.selectedItemId = R.id.mNavCamera
+                    }
+                    1 -> {
+
+                        navigationView.selectedItemId = R.id.mNavHome
+                    }
+                    2 -> {
+                        navigationView.selectedItemId = R.id.mNavFavourites
+                    }
+                    else -> navigationView.selectedItemId = R.id.mNavHome
+                }
+            }
+        })
+
+        view.navigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+//
+//      Navigating to the correct selected Item
+   private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.mNavCamera -> {
+                view_pager.currentItem = 0
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.mNavHome -> {
+                view_pager.currentItem = 1
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.mNavFavourites -> {
+                view_pager.currentItem = 2
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
     }
 
 }
